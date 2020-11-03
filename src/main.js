6eaa6e228d84cb6a0cfe91ed8dc103d31f38cb9b
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2020-11-03 07:33:05 -0300

    criando novos componentes

--- src/main.js
+++ src/main.js
@@ -1,52 +1,7 @@
 import Vue from 'vue'
 
-// Vue.component('lps-component', {
-//   data() {
-//     return {
-//       titulo: 'Curso de vuejs',
-//       contador: 0
-//     }
-//   },
-//   methods: {
-//     incrementar: function () {
-//       this.contador++
-//     }
-//   },
-//   template: `
-//   <div>
-//     <h1>{{titulo}}</h1>
-//     <button @click="incrementar"> Clicado {{ contador }} vezes</button> 
-//   </div>
-//   `
-// })
-
-const lpsComponent = {
-  data() {
-    return {
-      titulo: 'Curso de vuejs',
-      contador: 0
-    }
-  },
-  methods: {
-    incrementar: function () {
-      this.contador++
-    }
-  },
-  template: `
-  <div>
-    <h1>{{titulo}}</h1>
-    <button @click="incrementar"> Clicado {{ contador }} vezes</button> 
-  </div>
-  `
-}
+import App from './App.vue';
 
 new Vue({
-  el: '#app',
-  components: {
-    'lps-component': lpsComponent
-  }
-})
-
-new Vue({
-  el: '#app1',
-})
+  render: h => h(App)
+}).$mount('#app')
