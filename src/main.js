b78a79f5e5a9c7796658f2c2b107b5159d4ca68c
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2020-11-03 07:22:30 -0300

    registrando components globalmente e localmente

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 import Vue from 'vue'
 
-Vue.component('lps-component', {
+// Vue.component('lps-component', {
+//   data() {
+//     return {
+//       titulo: 'Curso de vuejs',
+//       contador: 0
+//     }
+//   },
+//   methods: {
+//     incrementar: function () {
+//       this.contador++
+//     }
+//   },
+//   template: `
+//   <div>
+//     <h1>{{titulo}}</h1>
+//     <button @click="incrementar"> Clicado {{ contador }} vezes</button> 
+//   </div>
+//   `
+// })
+
+const lpsComponent = {
   data() {
     return {
       titulo: 'Curso de vuejs',
@@ -18,8 +38,15 @@ Vue.component('lps-component', {
     <button @click="incrementar"> Clicado {{ contador }} vezes</button> 
   </div>
   `
-})
+}
 
 new Vue({
   el: '#app',
+  components: {
+    'lps-component': lpsComponent
+  }
+})
+
+new Vue({
+  el: '#app1',
 })
